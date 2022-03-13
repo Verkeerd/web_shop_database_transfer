e15a3142cb32ab55df09ad4ad1a344b0b84f5edb
verkeerd <susan.bruggeling@student.hu.nl>
2022-03-14 00:29:56 +0100

    updated absolute_biggest_difference() with the faster min_max version.

--- random_and_absolute_difference_products.py
+++ random_and_absolute_difference_products.py
@@ -16,15 +16,20 @@ def absolute_biggest_difference():
     products = load_sql.name_price_products()
     random_product = select_random_product(products)
 
-    products = support.binary_sort_matrix_lst(products, 0)
-    # TODO: Test if calculating the highest and lowest products would be quicker than sorting
+    # composes a list with all prices.
+    prices = support.select_column(products, 0)
+    # selects the product tuple with the lowest price
+    lowest_price_product = products[prices.index(min(prices))]
+    # selects the product tuple with the highest price
+    highest_price_product = products[prices.index(max(prices))]
+
     # calculates mid-value between the highest and lowest price.
-    true_median = (products[-1][0] - products[0][0]) / 2
+    true_median = (lowest_price_product[0] - highest_price_product[0]) / 2
     # returns the products with the highest price when the price of the random product is higher than true median.
     if random_product[0] > true_median:
-        return products[-1][1]
+        return highest_price_product[1]
     # returns the product with the lowest price otherwise.
-    return products[0][1]
+    return lowest_price_product[1]
 
 
 if __name__ == '__main__':
