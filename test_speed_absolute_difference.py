806fc8bb425c05fb80ec7cd0a063a5ec21e199b4
verkeerd <susan.bruggeling@student.hu.nl>
2022-03-26 00:23:59 +0100

    added code so test can still be run.

--- test_speed_absolute_difference.py
+++ test_speed_absolute_difference.py
@@ -3,6 +3,49 @@ from random_and_absolute_difference_products import select_random_product
 import time
 
 
+def binary_search_index_matrix(matrix, target, index, low, high):
+    """
+    searches for the index of the target in the column {index} of the data matrix.
+    args:
+        :param matrix: a matrix of data.
+        :param target: the element being sought
+        :param index: the column of the data matrix being sought.
+        :param low: the minimum index of the list that can contain the target.
+        :param high: the maximum index of the list that can contain the target.
+    returns:
+        :return int: returns the index the target was found at.
+                     returns the index the target would fit sorted when the index isn't found
+    """
+    if low > high:
+        return high + 1
+
+    mid_index = low + (high - low) // 2
+    try:
+        if target < matrix[mid_index][index]:
+            return binary_search_index_matrix(matrix, target, index=index, low=low, high=mid_index - 1)
+        elif target > matrix[mid_index][index]:
+            return binary_search_index_matrix(matrix, target, index=index, low=mid_index + 1, high=high)
+        elif matrix[mid_index][index] == target:
+            return mid_index
+    except TypeError:
+        return 0
+
+
+def binary_sort_matrix(matrix, index):
+    """
+    sorts a data matrix based upon the data in the index column of the matrix.
+    args:
+        :param matrix: a matrix of data.
+        :param index: the name of a column in the data matrix
+    returns:
+        :return: a copy of the matrix, sorted on the column {index}.
+    """
+    lst_sorted = [matrix[0]]
+    for i in range(1, len(matrix)):
+        lst_sorted.insert(binary_search_index_matrix(lst_sorted, matrix[i][index], index, 0, i - 1), matrix[i])
+    return lst_sorted
+
+
 def absolute_biggest_difference_sort():
     """
     Fetches a random product. Calculates which product has the biggest absolute difference in price with this random
@@ -12,7 +55,7 @@ def absolute_biggest_difference_sort():
     products = load_sql.name_price_products()
     random_product = select_random_product(products)
 
-    products = support.binary_sort_matrix_lst(products, 0)
+    products = binary_sort_matrix(products, 0)
 
     # calculates mid-value between the highest and lowest price.
     true_median = (products[-1][0] - products[0][0]) / 2
